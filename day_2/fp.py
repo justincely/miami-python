6a5c2a676aedcbaf107a85eabf66b7b09663e627
Victor Ng <victor@crankycoder.com>
2014-01-29 09:19:57 -0500

    added class based invocation of the FileReader

--- day_2/fp.py
+++ day_2/fp.py
@@ -8,9 +8,11 @@ example:
     python parser.py 2012_12_15.txt
 """
 
-import sys
 
 class FileReader(object):
+    def __init__(self):
+        self.count = 0
+
     def process(self, filename):
         """
         This a filereader
@@ -20,13 +22,15 @@ class FileReader(object):
 
         Returns a unique set of value specified by fieldname
         """
+        self.count = self.count + 1
+
         uniques = set()
         for line_no, line in enumerate(open(filename)):
             if line_no == 0:
                 # Skip the header
                 continue
             line_data = line.split()
-            city, state, shape = parse_record(line_no, line_data)
+            city, state, shape = self.parse_record(line_no, line_data)
             uniques.add(city)
         return uniques
 
@@ -36,10 +40,10 @@ class FileReader(object):
         record['date'] = data[0]
         record['time'] = data[1]
         record['city'] = data[2]
-        record['state'] = data[3] 
+        record['state'] = data[3]
         record['shape'] = data[4]
         return record['city'], record['state'], record['shape']
 
     def add(self, x, y):
+        self.count = self.count + 1
         return x + y
-
--- day_2/test_parser.py
+++ day_2/test_parser.py
@@ -1,16 +1,18 @@
 from nose import main
 from nose.tools import eq_
-import filereader
+import fp
 
 
 def test_filereader():
-    results = filereader.filereader('2012_12_15.txt')
+    myfp = fp.FileReader()
+    results = myfp.process('2012_12_15.txt')
     assert 'Lewiston' in results
     assert 'Forked_River' in results
 
 
 def test_add():
-    eq_(10, filereader.add(3, 9))
+    myfp = fp.FileReader()
+    eq_(12, myfp.add(3, 9))
 
 
 if __name__ == '__main__':
